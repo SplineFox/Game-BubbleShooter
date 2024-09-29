4524a693d606dd88588e8c4d9b49edb504d53b4e
SplineFox <betaspfox@gmail.com>
2024-09-29 18:07:19 +0900

    move BubbleTrajectory and BubbleTrajectoryPoint into separate files

---
+++ Assets/Scripts/BubbleTrajectory.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BubbleShooter
+{
+    public class BubbleTrajectory : IEnumerable<BubbleTrajectoryPoint>
+    {
+        private List<BubbleTrajectoryPoint> _points;
+        public BubbleTrajectoryPoint FirstPoint => _points.First();
+        public BubbleTrajectoryPoint LastPoint => _points.Last();
+
+        public BubbleTrajectory(BubbleTrajectoryPoint originPoint)
+        {
+            _points = new();
+            AddPoint(originPoint);
+        }
+
+        public void AddPoint(BubbleTrajectoryPoint point)
+        {
+            _points.Add(point);
+        }
+
+        public IEnumerator<BubbleTrajectoryPoint> GetEnumerator()
+        {
+            return _points.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _points.GetEnumerator();
+        }
+    }
+}
---
+++ Assets/Scripts/BubbleTrajectoryPoint.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace BubbleShooter
+{
+    public struct BubbleTrajectoryPoint
+    {
+        public Vector2 Position;
+        public Vector2 Direction;
+
+        public BubbleTrajectoryPoint(Vector2 position, Vector2 direction)
+        {
+            Position = position;
+            Direction = direction;
+        }
+    }
+}
